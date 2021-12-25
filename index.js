6577b6b3ddb17396149cfea39c081fe8ba4a5a5e
Shakib88 <shakibeeeuap28@gmail.com>
2021-12-25 14:58:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+// var x = 1412
+// var y = 12.34
+
+// console.log(x) // 1412
+// console.log(y) // 12.34
+
+//-----
+
+// Number()
+
+// var x = '4444'
+// var y = Number(x)
+// console.log(y); // 4444
+
+
+// var x = 'hello'
+// var y = Number(x)
+// console.log(y) //Nan
+
+// var x = Number("4444")
+// console.log(x) // 4444
+
+//-----
+
+// Number.parseInt()
+// Number.parseFloat()
+// Number.isFinite()
+// Number.isNaN()
+// Number.isSafeInteger()
+// Number.EPSILON
+// Number.MAX_SAFE_INTEGER
+// Number.MAX_VALUE
+// Number.MIN_VALUE
+// POSITIVE_INFINITY
+// NEGATIVE_INFINITY
+// Number.NaN
+
+// var x = Number.parseInt("4444.33");
+// console.log(x);
+
+ //console.log(Number.parseInt("4444.33"));
+ //console.log(parseInt("4444.33"));
+
+// var x = "4444.33"
+// console.log(parseInt(x));
+
+//  console.log(Number.MAX_SAFE_INTEGER);
+
+//-----
+
+// variable.toExponential()
+// variable.toFixed()
+// variable.toExponential()
+// variable.toExponential()
+// variable.toLocaleString()
+// variable.toPrecision()
+// variable.toString()
+
+
+// var x = 4444.33
+// console.log(x.toExponential());
+
+
+
+
+
+
+
+
+
+
+
